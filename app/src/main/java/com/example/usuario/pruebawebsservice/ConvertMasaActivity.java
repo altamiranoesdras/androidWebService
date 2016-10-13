ba68149724ff775eb2fdbf476ebf5e677b2856d2
Esdras Altamirano <altamiranoesdras@gmail.com>
2016-10-13 16:50:29 -0600

    Creado SOAP cliente para consumir servicio

--- app/src/main/java/com/example/usuario/pruebawebsservice/ConvertMasaActivity.java
+++ app/src/main/java/com/example/usuario/pruebawebsservice/ConvertMasaActivity.java
@@ -1,11 +1,18 @@
 package com.example.usuario.pruebawebsservice;
 
+import android.app.ProgressDialog;
+import android.os.Handler;
 import android.os.StrictMode;
+import android.support.annotation.StringDef;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.Toast;
+
+import com.example.usuario.pruebawebsservice.ws.SOAPClient;
 
 public class ConvertMasaActivity extends AppCompatActivity {
 
@@ -27,6 +34,11 @@ public class ConvertMasaActivity extends AppCompatActivity {
      */
     private String[] Units;
 
+    private Handler handler;
+    private ProgressDialog progressDialog;
+    private boolean ready;
+    private String convertedWeightValue;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -36,6 +48,8 @@ public class ConvertMasaActivity extends AppCompatActivity {
     }
 
     private void initComponents(){
+
+        handler = new Handler();
         etWeight = (EditText) findViewById(R.id.etWeight);
         spFromUnit = (Spinner) findViewById(R.id.spFromUnit);
         spToUnit = (Spinner) findViewById(R.id.spToUnit);
@@ -47,7 +61,35 @@ public class ConvertMasaActivity extends AppCompatActivity {
 
     }
 
-    public void convertWeight(){
+    public void convertWeight(View view){
+        progressDialog = ProgressDialog.show(this,"","Precessed request...");
+        Toast.makeText(this,"Weight: "+etWeight.getText().toString()+" from: "+(String) spFromUnit.getSelectedItem()+" to: "+(String) spToUnit.getSelectedItem(),Toast.LENGTH_LONG).show();
+        new Thread(){
+          @Override
+          public void run(){
+              ready = false;
+              convertedWeightValue = SOAPClient.convertWeight(etWeight.getText().toString(),(String) spFromUnit.getSelectedItem(),(String) spToUnit.getSelectedItem());
+              ready= true;
+          }
+        }.start();
 
+        handler.postDelayed(verifyConvertedWeight,1000);
+    }
+
+
+    private Runnable verifyConvertedWeight = new Runnable() {
+        @Override
+        public void run() {
+            if(ready){
+                showConvertedWeight();
+            }else{
+                handler.postDelayed(verifyConvertedWeight,500);
+            }
+        }
+    };
+
+    private void showConvertedWeight(){
+        progressDialog.cancel();
+        Toast.makeText(this,"Weight: "+convertedWeightValue,Toast.LENGTH_LONG).show();
     }
 }
--- app/src/main/java/com/example/usuario/pruebawebsservice/ws/SOAPClient.java
+++ app/src/main/java/com/example/usuario/pruebawebsservice/ws/SOAPClient.java
@@ -1,8 +1,53 @@
 package com.example.usuario.pruebawebsservice.ws;
 
+import android.util.Log;
+
+import org.ksoap2.SoapEnvelope;
+import org.ksoap2.serialization.SoapObject;
+import org.ksoap2.serialization.SoapPrimitive;
+import org.ksoap2.serialization.SoapSerializationEnvelope;
+import org.ksoap2.transport.HttpTransportSE;
+import org.xmlpull.v1.XmlPullParserException;
+
+import java.io.IOException;
+
 /**
  * Created by Usuario on 11/10/2016.
  */
 
 public class SOAPClient {
+    private  static String URL = "http://www.webservicex.net/convertweight.asmx";
+    private  static String METHOD_NAME = "ConvertWeight";
+    private  static String NAMESPACE = "http://www.webserviceX.NET/";
+    private  static String SOAP_ACTION = NAMESPACE + METHOD_NAME;
+
+    public static String convertWeight(String weight, String fromUnit,String toUnit){
+        String result= null;
+
+        SoapObject request = new SoapObject(NAMESPACE,METHOD_NAME);
+        request.addProperty("Weight",weight);
+        request.addProperty("FromUnit",fromUnit);
+        request.addProperty("ToUnit",toUnit);
+
+        SoapSerializationEnvelope envelope = new SoapSerializationEnvelope(SoapEnvelope.VER11);
+        envelope.dotNet= true;
+        envelope.setOutputSoapObject(request);
+
+        HttpTransportSE transportSE = new HttpTransportSE(URL);
+        transportSE.debug = true;
+        try{
+            transportSE.call(SOAP_ACTION, envelope);
+            SoapPrimitive soapPrimitive = (SoapPrimitive) envelope.getResponse();
+            result = soapPrimitive.toString();
+        } catch (XmlPullParserException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        Log.i("XML Request",transportSE.requestDump);
+        Log.i("XML Response",transportSE.responseDump);
+
+        return result;
+    }
 }
