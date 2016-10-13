31e0414c2d27f399e561829aa078c4cda1063a27
Esdras Altamirano <altamiranoesdras@gmail.com>
2016-10-13 13:19:22 -0600

    Codigo inicial funcionando y tarea terminada

--- app/src/main/java/com/example/usuario/pruebawebsservice/ConvertMasaActivity.java
+++ app/src/main/java/com/example/usuario/pruebawebsservice/ConvertMasaActivity.java
@@ -1,15 +1,31 @@
 package com.example.usuario.pruebawebsservice;
 
+import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.Spinner;
 
 public class ConvertMasaActivity extends AppCompatActivity {
 
+    /**
+     * Objeto controla el campo
+     */
     private EditText etWeight;
+    /**
+     * Objeto que controla el spinner
+     */
     private Spinner spFromUnit;
+    /**
+     * Objeto que controla el spinner
+     */
     private Spinner spToUnit;
+
+    /**
+     * Array de strings para guardar las unidades
+     */
+    private String[] Units;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +39,12 @@ public class ConvertMasaActivity extends AppCompatActivity {
         etWeight = (EditText) findViewById(R.id.etWeight);
         spFromUnit = (Spinner) findViewById(R.id.spFromUnit);
         spToUnit = (Spinner) findViewById(R.id.spToUnit);
+
+        Units = getResources().getStringArray(R.array.wight_units);
+
+        spFromUnit.setAdapter(new ArrayAdapter<String>(this,android.R.layout.simple_dropdown_item_1line,Units));
+        spToUnit.setAdapter(new ArrayAdapter<String>(this,android.R.layout.simple_dropdown_item_1line,Units));
+
     }
 
     public void convertWeight(){
