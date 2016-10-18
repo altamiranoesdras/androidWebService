1859e9a07c7ff293c2f0a89e86c6c76b3d7e3a62
Esdras Altamirano <altamiranoesdras@gmail.com>
2016-10-18 15:27:42 -0600

    RestFul Client Funcionando

--- app/src/main/java/com/example/usuario/pruebawebsservice/OptionsActivity.java
+++ app/src/main/java/com/example/usuario/pruebawebsservice/OptionsActivity.java
@@ -1,19 +1,80 @@
 package com.example.usuario.pruebawebsservice;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
+
+import com.example.usuario.pruebawebsservice.ws.RestFulClient;
 
 public class OptionsActivity extends AppCompatActivity {
+
+    private ProgressDialog progressDialog;
+    private Handler handler;
+    private  boolean ready;
+    private String serviceResult;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_options);
+
+        handler = new Handler();
     }
 
     public void goToSOAP(View view) {
         startActivity(new Intent(this,ConvertMasaActivity.class));
     }
+
+    public void consumoGet(View view){
+        progressDialog = ProgressDialog.show(this, "","Processed Request");
+
+        new Thread()   {
+            @Override
+            public void run(){
+                ready = false;
+                serviceResult = RestFulClient.connectToServerGet();
+                ready = true;
+            }
+        }.start();
+
+        handler.postDelayed(monitoringService,1000);
+    }
+
+    public void consumoPost(View view){
+        progressDialog = ProgressDialog.show(this, "","Processed Request");
+
+        new Thread()   {
+            @Override
+            public void run(){
+                ready = false;
+                serviceResult = RestFulClient.connectToServerPost();
+                ready = true;
+            }
+        }.start();
+
+        handler.postDelayed(monitoringService,1000);
+    }
+
+    private Runnable monitoringService = new Runnable() {
+        @Override
+        public void run() {
+            if(ready){
+                //Show Activity
+                showResult();
+            }else{
+                handler.postDelayed(monitoringService,1000);
+            }
+        }
+    };
+
+    private void  showResult(){
+        progressDialog.cancel();
+        Toast.makeText(this, serviceResult,Toast.LENGTH_LONG).show();
+    }
+
+
 }
--- app/src/main/java/com/example/usuario/pruebawebsservice/ws/RestFulClient.java
+++ app/src/main/java/com/example/usuario/pruebawebsservice/ws/RestFulClient.java
@@ -1,8 +1,113 @@
 package com.example.usuario.pruebawebsservice.ws;
 
+import android.util.Log;
+
+import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.URL;
+
 /**
  * Created by Usuario on 11/10/2016.
  */
 
 public class RestFulClient {
+    private  static final String GET_URL="http://asiti.powerwtechnology.com:9000/getService?";
+    private  static final String POST_URL="http://asiti.powerwtechnology.com:9000/postService?";
+
+    public static String connectToServerGet(){
+        String result = "";
+        try {
+            URL url = new URL(GET_URL);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+
+            connection.setConnectTimeout(10000);
+            connection.setReadTimeout(15000);
+            //connection.setDoInput(true);
+            //connection.setDoOutput(true);
+            connection.setRequestMethod("GET");
+    //            connection.setRequestProperty("Content-Type","application/x-www-form-urlencoded");
+
+            //OutputStream outputStream = new BufferedOutputStream(connection)
+
+            connection.connect();
+            int responseCode = connection.getResponseCode();
+            if(responseCode < HttpURLConnection.HTTP_BAD_REQUEST){
+                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                String temp = "";
+                result ="";
+                while ((temp = bufferedReader.readLine()) != null){
+                    result += temp;
+                }
+
+                Log.d("ServerConnection","jsonResponse"+ result);
+            }else if(responseCode == HttpURLConnection.HTTP_UNAUTHORIZED){
+                result= "HTTP Unautorized";
+            }else {
+                result= "HTTP Bad Request Message";
+            }
+
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (ProtocolException e) {
+            result = e.getMessage();
+            e.printStackTrace();
+        } catch (IOException e) {
+            result = e.getMessage();
+            e.printStackTrace();
+        }
+
+        return result;
+    }
+
+    public static String connectToServerPost(){
+        String result = "";
+        try {
+            URL url = new URL(POST_URL);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+
+            connection.setConnectTimeout(10000);
+            connection.setReadTimeout(15000);
+            connection.setDoInput(true);
+            connection.setDoOutput(true);
+            connection.setRequestMethod("POST");
+            connection.setRequestProperty("Content-Type","application/x-www-form-urlencoded");
+
+            //OutputStream outputStream = new BufferedOutputStream(connection)
+
+            connection.connect();
+            int responseCode = connection.getResponseCode();
+            if(responseCode < HttpURLConnection.HTTP_BAD_REQUEST){
+                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                String temp = "";
+                result ="";
+                while ((temp = bufferedReader.readLine()) != null){
+                    result += temp;
+                }
+
+                Log.d("ServerConnection","jsonResponse"+ result);
+            }else if(responseCode == HttpURLConnection.HTTP_UNAUTHORIZED){
+                result= "HTTP Unautorized";
+            }else {
+                result= "HTTP Bad Request Message";
+            }
+
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (ProtocolException e) {
+            result = e.getMessage();
+            e.printStackTrace();
+        } catch (IOException e) {
+            result = e.getMessage();
+            e.printStackTrace();
+        }
+
+        return result;
+    }
+
 }
